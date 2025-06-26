ca485a22b2d29e8483d4ee2d3e59d3b9a74652d8
harsha-cpp <sriharshatummalapalli@gmail.com>
2025-06-26 19:08:45 +0530

    Initial Commit

---
+++ DOTpomodoroTests/DOTpomodoroTests.swift
@@ -0,0 +1,17 @@
+//
+//  DOTpomodoroTests.swift
+//  DOTpomodoroTests
+//
+//  Created by Sri Harsha on 26/06/25.
+//
+
+import Testing
+@testable import DOTpomodoro
+
+struct DOTpomodoroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
